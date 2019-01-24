6955cc18dc69359ba6e4b03cf7eac345ca8f0b58
jiangzenghui005@163.com <jiangzenghui005@163.com>
2019-01-24 14:49:48 +0800

    Initial Commit

---
+++ TestRACCommandInTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRACCommandInTests
+//
+//  Created by ys on 2019/1/24.
+//  Copyright © 2019 mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestRACCommandInTests/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRACCommandInTests
+//
+//  Created by ys on 2019/1/24.
+//  Copyright © 2019 mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
